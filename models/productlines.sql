15c889e6bcd482bcb7d69d72b29ca4385c6ac6a9
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 11:00:22 +0000

    ncjgfxdhf

--- models/productlines.sql
+++ models/productlines.sql
@@ -1,28 +1,57 @@
 {{ config(
     materialized='incremental',
-    unique_key='de_productline_id'  
+    unique_key='productline'
 ) }}
 
-{% set etl_batch_no = 1001 %}
-{% set etl_batch_date = '2001-01-01' %}
+WITH source_data AS (
+    SELECT
+        productline,
+        create_timestamp,
+        update_timestamp,
+        1001 AS etl_batch_no,
+        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date
+    FROM
+        devstage.productlines
+),
 
-INSERT INTO devdw.productlines (
-    productLine,
-    src_create_timestamp,
-    src_update_timestamp,
-    dw_create_timestamp,
-    dw_update_timestamp,
-    etl_batch_no,
-    etl_batch_date
+existing_data AS (
+    SELECT
+        productline,
+        src_create_timestamp,
+        src_update_timestamp,
+        etl_batch_no,
+        etl_batch_date,
+        dw_update_timestamp,
+        dw_product_line_id
+    FROM
+        devdw.productlines  -- Refers to the current state of the table created by dbt
+),
+
+ranked_data AS (
+    SELECT
+        source_data.productline,
+        ROW_NUMBER() OVER (ORDER BY source_data.productline) + COALESCE(MAX(existing_data.dw_product_line_id) OVER (), 0) AS dw_product_line_id,
+        CASE
+            WHEN source_data.productline IS NOT NULL AND existing_data.productline IS NULL THEN source_data.create_timestamp
+            ELSE existing_data.src_create_timestamp
+        END AS src_create_timestamp,
+        COALESCE(source_data.update_timestamp, existing_data.src_update_timestamp) AS src_update_timestamp,
+        1001 AS etl_batch_no,
+        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date,
+        CASE
+            WHEN source_data.productline IS NOT NULL THEN CURRENT_TIMESTAMP
+            ELSE existing_data.dw_update_timestamp
+        END AS dw_update_timestamp,
+        CURRENT_TIMESTAMP AS dw_create_timestamp
+    FROM
+        source_data
+    LEFT JOIN existing_data ON source_data.productline = existing_data.productline
 )
-SELECT
-    st.productLine,
-    COALESCE(dw.src_create_timestamp, st.create_timestamp) AS src_create_timestamp,
-    st.update_timestamp AS src_update_timestamp,
-    COALESCE(dw.dw_create_timestamp, CURRENT_TIMESTAMP) AS dw_create_timestamp,
-    CURRENT_TIMESTAMP AS dw_update_timestamp,
-    {{ etl_batch_no }} AS etl_batch_no,
-    '{{ etl_batch_date }}'::DATE AS etl_batch_date
-FROM {{ source('devstage', 'productlines') }} AS st
-LEFT JOIN devdw.productlines AS dw
-ON st.productLine = dw.productLine;
+
+SELECT *
+FROM ranked_data
+
+{% if is_incremental() %}
+WHERE
+    ranked_data.productline IS NOT NULL  -- Only process new or updated rows
+{% endif %}
