1083ccddf31cad6a30ff722d1723316507c5b618
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 10:22:53 +0000

    nc

--- models/productlines.sql
+++ models/productlines.sql
@@ -1,34 +1,37 @@
 {{ config(
     materialized='incremental',  
-    unique_key='dw_productline_id'     
+    unique_key='dw_productline_id'  
 ) }}
-
 
 {% set etl_batch_no = 1001 %}
 {% set etl_batch_date = '2001-01-01' %}
 
-
-
-
-
-INSERT INTO devdw.productlines (
-    productLine,
-    src_create_timestamp,
-    src_update_timestamp,
-    dw_create_timestamp,
-    dw_update_timestamp,
-    etl_batch_no,
-    etl_batch_date
-)
-SELECT 
-    st.productLine,
-    st.create_timestamp,
-    st.update_timestamp,
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP,
-    CAST({{ etl_batch_no }} AS INTEGER),
-    CAST({{ etl_batch_date }} AS DATE)
-FROM devstage.productlines AS st
-LEFT JOIN devdw.productlines AS dw
-ON st.productLine = dw.productLine
-WHERE dw.productLine IS NULL;
+-- Use a single UPSERT operation (MERGE equivalent in Redshift)
+MERGE INTO devdw.productlines AS target
+USING devstage.productlines AS source
+ON target.productLine = source.productLine
+WHEN MATCHED THEN
+    UPDATE SET
+        target.src_update_timestamp = source.update_timestamp,
+        target.dw_update_timestamp = CURRENT_TIMESTAMP,
+        target.etl_batch_no = CAST({{ etl_batch_no }} AS INTEGER),
+        target.etl_batch_date = CAST({{ etl_batch_date }} AS DATE)
+WHEN NOT MATCHED THEN
+    INSERT (
+        productLine,
+        src_create_timestamp,
+        src_update_timestamp,
+        dw_create_timestamp,
+        dw_update_timestamp,
+        etl_batch_no,
+        etl_batch_date
+    )
+    VALUES (
+        source.productLine,
+        source.create_timestamp,
+        source.update_timestamp,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP,
+        CAST({{ etl_batch_no }} AS INTEGER),
+        CAST({{ etl_batch_date }} AS DATE)
+    );
