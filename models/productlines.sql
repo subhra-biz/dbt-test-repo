ab7fc2e0973bcb836d9193abe4dbd6bc2994c35c
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 09:53:34 +0000

    new commit

--- models/productlines.sql
+++ models/productlines.sql
@@ -1,37 +1,36 @@
 {{ config(
-    schema='devdw',
-    materialized='incremental',  
-    unique_key='productLine'     
+    materialized='incremental',
+    unique_key='productLine',
+    incremental_strategy='merge'
 ) }}
 
--- Define batch variables
-{% set etl_batch_no = var('etl_batch_no', '1') %}
-{% set etl_batch_date = var('etl_batch_date', '2024-12-06') %}
-
--- Update existing records
 WITH updated_records AS (
     SELECT 
         st.update_timestamp AS src_update_timestamp,
         CURRENT_TIMESTAMP AS dw_update_timestamp,
-        CAST({{ etl_batch_no }} AS INTEGER) AS etl_batch_no,
-        CAST({{ etl_batch_date }} AS DATE) AS etl_batch_date,
+        CAST(1 AS INTEGER) AS etl_batch_no,
+        CAST('2024-12-06' AS DATE) AS etl_batch_date,
         st.productLine
-    FROM {{ source('devstage', 'productlines') }} AS st
-    JOIN {{ this }} AS dw
+    FROM devstage.productlines AS st
+    JOIN devdw.productlines AS dw
     ON st.productLine = dw.productLine
 )
--- Apply updates
-UPDATE {{ this }}
+-- Update existing records
+UPDATE devdw.productlines
 SET 
     src_update_timestamp = updated_records.src_update_timestamp,
     dw_update_timestamp = updated_records.dw_update_timestamp,
     etl_batch_no = updated_records.etl_batch_no,
     etl_batch_date = updated_records.etl_batch_date
 FROM updated_records
-WHERE {{ this }}.productLine = updated_records.productLine;
+WHERE devdw.productlines.productLine = updated_records.productLine
+
+{% if is_incremental() %}
+    AND updated_records.productLine IS NOT NULL
+{% endif %}
 
 -- Insert new records
-INSERT INTO {{ this }} (
+INSERT INTO devdw.productlines (
     productLine,
     src_create_timestamp,
     src_update_timestamp,
@@ -46,10 +45,13 @@ SELECT
     st.update_timestamp,
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP,
-    CAST({{ etl_batch_no }} AS INTEGER),
-    CAST({{ etl_batch_date }} AS DATE)
-FROM {{ source('devstage', 'productlines') }} AS st
-LEFT JOIN {{ this }} AS dw
+    CAST(1 AS INTEGER),
+    CAST('2024-12-06' AS DATE)
+FROM devstage.productlines AS st
+LEFT JOIN devdw.productlines AS dw
 ON st.productLine = dw.productLine
-WHERE dw.productLine IS NULL;
+WHERE dw.productLine IS NULL
 
+{% if is_incremental() %}
+    AND st.productLine IS NOT NULL
+{% endif %}
