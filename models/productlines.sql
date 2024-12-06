3fee166344526c99b5f877b771bdac9f0406db9c
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 10:14:29 +0000

    new commit

--- models/productlines.sql
+++ models/productlines.sql
@@ -9,29 +9,6 @@
 
 -- Define the logic for updating and inserting records
 
-WITH updated_records AS (
-    SELECT 
-        st.update_timestamp AS src_update_timestamp,
-        CURRENT_TIMESTAMP AS dw_update_timestamp,
-        CAST({{ etl_batch_no }} AS INTEGER) AS etl_batch_no,
-        CAST({{ etl_batch_date }} AS DATE) AS etl_batch_date,
-        st.productLine
-    FROM devstage.productlines AS st
-    JOIN devdw.productlines AS dw
-    ON st.productLine = dw.productLine
-)
-
-{% if is_incremental() %}
-    -- Update existing records (only if the model is running in incremental mode)
-    UPDATE devdw.productlines
-    SET 
-        src_update_timestamp = updated_records.src_update_timestamp,
-        dw_update_timestamp = updated_records.dw_update_timestamp,
-        etl_batch_no = updated_records.etl_batch_no,
-        etl_batch_date = updated_records.etl_batch_date
-    FROM updated_records
-    WHERE devdw.productlines.productLine = updated_records.productLine;
-{% endif %}
 
 -- Insert new records (only if they do not exist in the target table)
 INSERT INTO devdw.productlines (
