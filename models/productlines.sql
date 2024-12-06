64f89162555f54f3a158f8197fecef8b82516db7
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 10:51:50 +0000

    nc

--- models/productlines.sql
+++ models/productlines.sql
@@ -1,33 +1,11 @@
 {{ config(
     materialized='incremental',
-    unique_key='productLine'  
+    unique_key='de_productline_id'  
 ) }}
 
 {% set etl_batch_no = 1001 %}
 {% set etl_batch_date = '2001-01-01' %}
 
--- Step 1: Update existing records
-WITH updated_records AS (
-    SELECT 
-        st.productLine,
-        st.update_timestamp AS src_update_timestamp,
-        CURRENT_TIMESTAMP AS dw_update_timestamp,
-        CAST({{ etl_batch_no }} AS INTEGER) AS etl_batch_no,
-        CAST('{{ etl_batch_date }}' AS DATE) AS etl_batch_date
-    FROM devstage.productlines AS st
-    INNER JOIN devdw.productlines AS dw
-    ON st.productLine = dw.productLine
-)
-UPDATE devdw.productlines
-SET 
-    src_update_timestamp = updated_records.src_update_timestamp,
-    dw_update_timestamp = updated_records.dw_update_timestamp,
-    etl_batch_no = updated_records.etl_batch_no,
-    etl_batch_date = updated_records.etl_batch_date
-FROM updated_records
-WHERE devdw.productlines.productLine = updated_records.productLine;
-
--- Step 2: Insert new records
 INSERT INTO devdw.productlines (
     productLine,
     src_create_timestamp,
@@ -37,15 +15,14 @@ INSERT INTO devdw.productlines (
     etl_batch_no,
     etl_batch_date
 )
-SELECT 
+SELECT
     st.productLine,
-    st.create_timestamp,
-    st.update_timestamp,
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP,
-    CAST({{ etl_batch_no }} AS INTEGER),
-    CAST('{{ etl_batch_date }}' AS DATE)
-FROM devstage.productlines AS st
+    COALESCE(dw.src_create_timestamp, st.create_timestamp) AS src_create_timestamp,
+    st.update_timestamp AS src_update_timestamp,
+    COALESCE(dw.dw_create_timestamp, CURRENT_TIMESTAMP) AS dw_create_timestamp,
+    CURRENT_TIMESTAMP AS dw_update_timestamp,
+    {{ etl_batch_no }} AS etl_batch_no,
+    '{{ etl_batch_date }}'::DATE AS etl_batch_date
+FROM {{ source('devstage', 'productlines') }} AS st
 LEFT JOIN devdw.productlines AS dw
-ON st.productLine = dw.productLine
-WHERE dw.productLine IS NULL;
+ON st.productLine = dw.productLine;
