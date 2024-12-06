e2bdd5b5751e7ae7df811efcda37f0cfe3c7327f
subhrajyoti.nayak@bizacuity.com <subhrajyoti.nayak@bizacuity.com>
2024-12-06 10:01:05 +0000

    new commit

--- models/productlines.sql
+++ models/productlines.sql
@@ -1,35 +1,39 @@
 {{ config(
-    materialized='incremental',
-    unique_key='productLine',
-    incremental_strategy='merge'
+    materialized='incremental',  
+    unique_key='productLine'     
 ) }}
+
+-- Define the ETL batch number and batch date dynamically or statically
+{% set etl_batch_no = 1001 %}
+{% set etl_batch_date = '2001-01-01' %}
+
+-- Define the logic for updating and inserting records
 
 WITH updated_records AS (
     SELECT 
         st.update_timestamp AS src_update_timestamp,
         CURRENT_TIMESTAMP AS dw_update_timestamp,
-        CAST(1 AS INTEGER) AS etl_batch_no,
-        CAST('2024-12-06' AS DATE) AS etl_batch_date,
+        CAST({{ etl_batch_no }} AS INTEGER) AS etl_batch_no,
+        CAST({{ etl_batch_date }} AS DATE) AS etl_batch_date,
         st.productLine
     FROM devstage.productlines AS st
     JOIN devdw.productlines AS dw
     ON st.productLine = dw.productLine
 )
--- Update existing records
-UPDATE devdw.productlines
-SET 
-    src_update_timestamp = updated_records.src_update_timestamp,
-    dw_update_timestamp = updated_records.dw_update_timestamp,
-    etl_batch_no = updated_records.etl_batch_no,
-    etl_batch_date = updated_records.etl_batch_date
-FROM updated_records
-WHERE devdw.productlines.productLine = updated_records.productLine
 
 {% if is_incremental() %}
-    AND updated_records.productLine IS NOT NULL
+    -- Update existing records (only if the model is running in incremental mode)
+    UPDATE devdw.productlines
+    SET 
+        src_update_timestamp = updated_records.src_update_timestamp,
+        dw_update_timestamp = updated_records.dw_update_timestamp,
+        etl_batch_no = updated_records.etl_batch_no,
+        etl_batch_date = updated_records.etl_batch_date
+    FROM updated_records
+    WHERE devdw.productlines.productLine = updated_records.productLine;
 {% endif %}
 
--- Insert new records
+-- Insert new records (only if they do not exist in the target table)
 INSERT INTO devdw.productlines (
     productLine,
     src_create_timestamp,
@@ -45,13 +49,9 @@ SELECT
     st.update_timestamp,
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP,
-    CAST(1 AS INTEGER),
-    CAST('2024-12-06' AS DATE)
+    CAST({{ etl_batch_no }} AS INTEGER),
+    CAST({{ etl_batch_date }} AS DATE)
 FROM devstage.productlines AS st
 LEFT JOIN devdw.productlines AS dw
 ON st.productLine = dw.productLine
-WHERE dw.productLine IS NULL
-
-{% if is_incremental() %}
-    AND st.productLine IS NOT NULL
-{% endif %}
+WHERE dw.productLine IS NULL;
